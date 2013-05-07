d4ca9482024d0b264d29f418944fa41c94e6ff47
Charles-Elie <admin@hybridnocorp.com>
2013-05-07 11:34:49 +0200

    [BackupCommand] all arguments are now optional options

--- Command/BackupCommand.php
+++ Command/BackupCommand.php
@@ -13,20 +13,20 @@ class BackupCommand extends BaseCommand
     {
         $this
             ->setName('database:backup')
-            ->setDefinition(array(
-                new InputArgument('connection-service-id', InputArgument::REQUIRED, 'The connection service ID of the database from which you want to generate a backup sql file.'),
-                new InputArgument('target-dir', InputArgument::REQUIRED, 'The directory where the backup file will be saved.'),
-                new InputArgument('filename', InputArgument::OPTIONAL, 'The name for the backup file.')
-            ))
+            ->setDescription('Import the worklists from csv file')
+            ->addOption('connection-service-id', null, InputOption::VALUE_OPTIONAL, 'The connection service ID of the database from which you want to generate a backup sql file. (default is "doctrine.dbal.default_connection")', 'doctrine.dbal.default_connection')
+            ->addOption('target-dir',            null, InputOption::VALUE_OPTIONAL, 'The directory where the backup file will be saved. (default is {root_dir}/backups)')
+            ->addOption('filename',              null, InputOption::VALUE_OPTIONAL, 'The name for the backup file.')
             ->setHelp(<<<EOT
 The <info>database:backup</info> command generates a backup of a 
 database using the connection you choose. Note that the database platform 
 must be supported by the bundle. Check if the database platform you want 
 to generate a backup from is supported.
 
-An example of usage of the command:
+Examples of usage of the command:
 
-<info>./app/console database:backup "my-connection-service-id" "/var/tmp" [my_sql_file_name.sql]</info>
+<info>./app/console database:backup</info>
+<info>./app/console database:backup --conection-service-id="my-connection-service-id" target-dir="/var/tmp" filename="my_sql_file_name.sql"</info>
 
 EOT
             )
@@ -38,9 +38,15 @@ EOT
         $container = $this->getContainer();
         
         $factory = $container->get('backup_restore.factory');
-        $connectionServiceId = $input->getArgument('connection-service-id');
-        $directory = $input->getArgument('target-dir');
-        $fileName = $input->getArgument('filename') ? $input->getArgument('filename') : null;
+        $connectionServiceId = $input->getOption('connection-service-id');
+        $directory = $input->getOption('target-dir');
+        
+        if(!$directory)
+        {
+            $directory = $container->getParameter('kernel.root_dir') . '/backups';
+        }
+        
+        $fileName = $input->getOption('filename') ? $input->getOption('filename') : null;
         
         $backupInstance = $factory->getBackupInstance($connectionServiceId);
         
